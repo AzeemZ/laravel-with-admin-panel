8f2ceeb624f67c4595eb5714cae7007d4c304ed5
shapack <azeem3z123@gmail.com>
2019-05-18 23:42:14 +0500

    routes added for admin panel

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,28 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function (){
+    Route::get('/dashboard', 'AdminController@index')->name('dashboard');
+    Route::get('/dashboard2', 'AdminController@index2');
+    Route::get('/login', 'AdminController@login');
+    Route::get('/register', 'AdminController@register');
+    Route::get('/charts', 'AdminController@charts');
+    Route::get('/error-403', 'AdminController@error403');
+    Route::get('/error-404', 'AdminController@error404');
+    Route::get('/error-405', 'AdminController@error405');
+    Route::get('/error-500', 'AdminController@error500');
+    Route::get('/form-basic', 'AdminController@formBasic');
+    Route::get('/form-wizard', 'AdminController@formWizard');
+    Route::get('/grid', 'AdminController@grid');
+    Route::get('/icon-fontawesome', 'AdminController@fontAwesomeIcons');
+    Route::get('/icon-material', 'AdminController@materialIcons');
+    Route::get('/buttons', 'AdminController@buttons');
+    Route::get('/calendar', 'AdminController@calendar');
+    Route::get('/chat', 'AdminController@chat');
+    Route::get('/elements', 'AdminController@elements');
+    Route::get('/gallery', 'AdminController@gallery');
+    Route::get('/invoice', 'AdminController@invoice');
+    Route::get('/tables', 'AdminController@tables');
+    Route::get('/widgets', 'AdminController@widgets');
+});
