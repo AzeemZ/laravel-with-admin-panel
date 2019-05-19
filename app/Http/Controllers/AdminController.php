39e15978a0adc3925eec5c8ad1de89ed20039461
shapack <azeem3z123@gmail.com>
2019-05-19 12:12:11 +0500

    admin controller created

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function index2()
+    {
+        //
+    }
+
+    public function login()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+
+    public function charts()
+    {
+        //
+    }
+
+    public function error403()
+    {
+        //
+    }
+
+    public function error404()
+    {
+        //
+    }
+
+    public function error405()
+    {
+        //
+    }
+
+    public function error500()
+    {
+        //
+    }
+
+    public function formBasic()
+    {
+        //
+    }
+
+    public function formWizard()
+    {
+        //
+    }
+
+    public function grid()
+    {
+        //
+    }
+
+    public function fontAwesomeIcons()
+    {
+        //
+    }
+
+    public function materialIcons()
+    {
+        //
+    }
+
+    public function buttons()
+    {
+        //
+    }
+
+    public function calendar()
+    {
+        //
+    }
+
+    public function chat()
+    {
+        //
+    }
+
+    public function elements()
+    {
+        //
+    }
+
+    public function gallery()
+    {
+        //
+    }
+
+    public function invoice()
+    {
+        //
+    }
+
+    public function tables()
+    {
+        //
+    }
+
+    public function widgets()
+    {
+        //
+    }
+}
